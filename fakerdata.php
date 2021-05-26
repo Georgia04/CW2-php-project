84a24a6e184c841cfbe3bd942e1e748723fd4777
georgia <60743148+Georgia04@users.noreply.github.com>
2021-05-26 05:58:18 -1200

    Task 1

---
+++ fakerdata.php
@@ -0,0 +1,19 @@
+<?php
+include("_includes/dbconnect.inc");
+
+require_once 'vendor/autoload.php';
+
+$faker = Faker\Factory::create('en_GB');
+
+for($i = 0; $i < 10; $i++) {
+    $query = "INSERT INTO student (studentid, password, dob, firstname, lastname, house, town,
+    county, country, postcode)
+    VALUES ('". $faker->randomNumber($nbDigits = 8, $strict = true)."', '".$faker->password."', '".$faker->date($format = 'Y-m-d', $max = 'now')."', '".$faker->firstName($gender = null|'male'|'female')."', 
+    '".$faker->lastName."', '".$faker->streetAddress."', '".$faker->city."', '".$faker->county."', '".$faker->country."', '".$faker->postcode."' )";
+
+    mysqli_query($conn, $query);
+}
+
+echo "Success";
+
+?>
