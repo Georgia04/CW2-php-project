7965caafc95fa1074c609a842306c5aba42f48b0
georgia <60743148+Georgia04@users.noreply.github.com>
2021-06-01 22:38:44 -1200

    faker update

--- faker.php
+++ faker.php
@@ -9,7 +9,7 @@ for($i = 0; $i < 10; $i++) {
     $query = "INSERT INTO student (studentid, password, dob, firstname, lastname, house, town,
     county, country, postcode)
     VALUES ('". $faker->randomNumber($nbDigits = 8, $strict = true)."', '".$faker->password."', '".$faker->date($format = 'Y-m-d', $max = 'now')."', '".$faker->firstName($gender = null|'male'|'female')."', 
-    '".$faker->lastName."', '".$faker->streetAddress."', '".$faker->city."', '".$faker->county."', '".$faker->country."', '".$faker->postcode."' )";
+    '".$faker->lastName."', '".$faker->streetAddress."', '".$faker->town."', '".$faker->county."', '".$faker->country."', '".$faker->postcode."' )";
 
     mysqli_query($conn, $query);
 }
